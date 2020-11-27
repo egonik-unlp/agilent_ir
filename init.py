fab8899977876f0df0d02819878050963c169469
Eduardo Gonik <gonik@quimica.unlp.edu.ar>
2020-11-27 18:30:42 -0300

    segunda funcion

--- init.py
+++ init.py
@@ -1,15 +1,28 @@
 import numpy as np
 import csv
 import os
+
 def parse_file(file):
     with open(file, 'r') as f:
+        conv = [int, float, float]
         reader = csv.reader(f)
-        param = [int(next(reader)[0]) for i in range(3)]
-    return np.linspace(param[1], param[2], param[0])
+        param = [con(next(reader)[0]) for con,i in zip(conv,range(3))]
+        trash = [next(reader) for i in range(3)]
+        wavenumber = np.linspace(param[1], param[2], int(param[0]))
+        transmittance = [float(line[0]) for line in reader]
+    return {'wavenumber': wavenumber, 'transmittance': transmittance } 
 
+def parse_folder(path = '.', filter_asp = True, one_dict = True):
+    if filter_asp:
+        files = [i for i in os.listdir(path = path) if '.asp' in i[-4:]]
+    else:
+        files = [i for i in os.listdir(path = path)]
+    if one_dict:
+        {'wavenumber': parse_file(files[0])['wavenumber'], file: trans for file, trans in zip(files,[])}
+    
 
 lista = os.listdir(path = '/home/gonik/Documents/agilent_ir/test_data/IR')
-
+os.chdir('/home/gonik/Documents/agilent_ir/test_data/IR')
 files = [i for i in lista if '.asp' in i[-4:]]
 
 x = parse_file(files[1])
