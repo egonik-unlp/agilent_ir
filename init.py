ea7e2ce510ea1930b8fc674d0829aeb4fc1d21a0
Eduardo Gonik <gonik@quimica.unlp.edu.ar>
2020-11-27 18:19:20 -0300

    primera funcion

--- init.py
+++ init.py
@@ -1,4 +1,15 @@
 import numpy as np
+import csv
+import os
 def parse_file(file):
     with open(file, 'r') as f:
-        for line in file
+        reader = csv.reader(f)
+        param = [int(next(reader)[0]) for i in range(3)]
+    return np.linspace(param[1], param[2], param[0])
+
+
+lista = os.listdir(path = '/home/gonik/Documents/agilent_ir/test_data/IR')
+
+files = [i for i in lista if '.asp' in i[-4:]]
+
+x = parse_file(files[1])
