4a169288790b597cb26c3cd4b0313933682469ae
Eduardo Gonik <gonik@quimica.unlp.edu.ar>
2020-11-28 18:33:46 -0300

    added small functionalities

--- agilent.py
+++ agilent.py
@@ -17,6 +17,8 @@ class spectrum():
         self.transmittance = 0
         self.dict = 0 
         self.file = 0
+    def __len__(self):
+        return len(self.transmittance)
     def load_files(self, file):
         self.name = file[:-4]
         self.file = file
@@ -50,14 +52,25 @@ class spectra(spectrum):
         super().__init__()
         self.index  = []
         self.columns = {}
-        
+        self.spectra_list = 0
+        self.name_list = None
+    def __iter__(self):
+        return self.spectra_list.__iter__()
+    def __len__(self):
+        return len(self.spectra_list)
+    
+    def rename(self,ft):
+        for sp, name in zip(self.spectra_list, self. name_list):
+            sp.name = name[:-len(ft)] 
         
     def import_dir(self,directory, ft = '.asp'):
         file_list = [file for file in os.listdir(directory) if ft in file[-len(ft):]]
         path_list = [os.path.join(directory, file) for file in file_list ]
-        spectra_list = [load_file(file) for file in path_list]
-        self.columns =  {file[:-len(ft)] :sp.transmittance for file, sp in zip(file_list, spectra_list)}
-        self.index = np.array(spectra_list[0].wavenumber)
+        self.name_list = file_list.copy()
+        self.spectra_list = [load_file(file) for file in path_list]
+        self.rename(ft = ft)
+        self.columns =  {sp.name:sp.transmittance for sp in self.spectra_list}
+        self.index = np.array(self.spectra_list[0].wavenumber)
         
     def as_pandas(self):
         return pd.DataFrame(data = self.columns)
