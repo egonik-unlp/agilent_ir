33febd7b3d8c00571370a20d467afdaf9161b7cb
Eduardo Gonik <gonik@quimica.unlp.edu.ar>
2020-11-29 20:22:11 -0300

    improved file structure

--- agilentaspparser/__init__.py
+++ agilentaspparser/__init__.py
@@ -63,8 +63,8 @@ class spectra(spectrum):
         
     def import_obj(self,object, ft = '.asp'):
         if type(object) == str:
-            file_list = [file for file in os.listdir(directory) if ft in file[-len(ft):]]
-            path_list = [os.path.join(directory, file) for file in file_list ]
+            file_list = [file for file in os.listdir(object) if ft in file[-len(ft):]]
+            path_list = [os.path.join(object, file) for file in file_list ]
             self.name_list = file_list.copy()
             self.spectra_list = [load_file(file) for file in path_list]
             self.rename(ft = ft)
