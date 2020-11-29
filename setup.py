cf10754dc916a14bc2ed3581745ef9f6368bd868
Eduardo Gonik <gonik@quimica.unlp.edu.ar>
2020-11-29 00:24:49 -0300

    Functional package. Extremely poor documentation.

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r") as fh:
 
 setuptools.setup(
     name="agilentaspparser", # Replace with your own username
-    version="0.0.1",
+    version="0.0.2",
     author="Eduardo Gonik",
     author_email="gonik@quimica.unlp.edu.ar",
     description="Parser for Agilent IR .asp files",
