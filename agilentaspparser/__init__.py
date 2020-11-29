8cfa94879bc3f616d60a70fdcd223d3f72a61fec
Eduardo Gonik <gonik@quimica.unlp.edu.ar>
2020-11-29 15:15:37 -0300

    tidied import procedure for spectra

--- agilentaspparser/__init__.py
+++ agilentaspparser/__init__.py
@@ -72,12 +72,6 @@ class spectra(spectrum):
             self.spectra_list = [load_file(file) for file in file_list if ft in file[-len(ft):]]
         self.columns =  {sp.name:sp.transmittance for sp in self.spectra_list}
         self.index = np.array(self.spectra_list[0].wavenumber)
-
-    def import_list(self, file_list, ft = '.asp'):
-        self.spectra_list = [load_file(file) for file in file_list if ft in file[-len(ft):]]
-        self.columns =  {sp.name:sp.transmittance for sp in self.spectra_list}
-        self.index = np.array(self.spectra_list[0].wavenumber)
-
         
     def as_pandas(self):
         return pd.DataFrame(data = self.columns, index = self.index)
